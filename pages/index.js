6bf89a5384b5f4b2a0bfa5656098529ffaa4fca8
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-01-09 00:01:07 +0700

    aboutme

--- pages/index.js
+++ pages/index.js
@@ -149,11 +149,14 @@ export default function Index() {
       <section id="about" className="bg-stone-900 py-24">
         <h1 className="text-7xl text-center mb-12">About Me</h1>
         <p className="text-3xl mx-auto w-1/2">
-          I'm a student who has a passion for creating a game as a hobby.
+          {"I'm a student who has a passion for creating a game as a hobby."}
           <br></br>
-          So I will turn a hobby into a larger, more targeted project and
-          challenge
-          <br></br>and always be open to learning and improving my skills.
+          {
+            "So I will turn a hobby into a larger, more targeted project and\
+          challenge"
+          }
+          <br></br>
+          {"and always be open to learning and improving my skills."}
         </p>
       </section>
       {/* test */}
