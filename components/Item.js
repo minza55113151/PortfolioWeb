825b596009c3b04750be1c1fa0455a0947f029a4
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-21 20:19:18 +0700

    add description

--- components/Item.js
+++ components/Item.js
@@ -1,10 +1,15 @@
 import Icon from "./Icon";
 
-export default function Item({ text, src, size }) {
+export default function Item({ text, src, size, description, children }) {
   return (
     <div className="flex content-center">
       <Icon src={src} size={size}></Icon>
-      <h1 className="text-lg sm:text-2xl align-middle mx-5">{text}</h1>
+      <div>
+        <h1 className="text-lg sm:text-2xl align-middle mx-5">{text}</h1>
+        {description && (
+          <h1 className="text-lg sm:text-1xl mx-8">{"- " + description}</h1>
+        )}
+      </div>
     </div>
   );
 }
