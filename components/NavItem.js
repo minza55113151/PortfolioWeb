7f2fed2de819af4dc0db730f9998d9e04342a339
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-21 20:19:32 +0700

    add px-4

--- components/NavItem.js
+++ components/NavItem.js
@@ -1,7 +1,7 @@
 export default function NavItem({ href, children }) {
   return (
     <li>
-      <a href={href} className="text-sm sm:text-xl hover:text-red-500">
+      <a href={href} className="text-sm sm:text-xl hover:text-red-500 px-4">
         {children}
       </a>
     </li>
