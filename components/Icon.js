664d62e29c2d597180e5e98a1f0510540ff1e692
MinZung <90918826+minza55113151@users.noreply.github.com>
2023-02-21 20:19:01 +0700

    add div for protect unsync image size

--- components/Icon.js
+++ components/Icon.js
@@ -1,3 +1,7 @@
 export default function Icon({ src, size = 50 }) {
-  return <img src={src} alt="Icon" width={size} className="aspect-auto"></img>;
+  return (
+    <div>
+      <img src={src} alt="Icon" width={size} className="aspect-auto"></img>
+    </div>
+  );
 }
